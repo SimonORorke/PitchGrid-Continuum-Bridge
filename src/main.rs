8c4b6951eec66e396a870684afbbe2cbb50f4ecf
Simon O'Rorke <simon.ororke@outlook.com>
2026-01-17 14:00:44 +1300

    callback midi_input_changed

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ slint::include_modules!();
 fn main() {
     // use slint::Model; // For for loops on Slint components.
     let main_window = MainWindow::new().unwrap();
-    main_window.set_window_title("PitchGrid-Continuum Companion".into()); // SOR
+    main_window.set_window_title("PitchGrid-Continuum Companion".into());
+
+    main_window.on_midi_input_changed(|index| {
+        println!("MIDI input changed to {}", index);
+    });
+
     main_window.run().unwrap();
 }
