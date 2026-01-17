2d4d8cb81f77bdd9e0011df1f4052fca2a316032
Simon O'Rorke <simon.ororke@outlook.com>
2026-01-18 09:40:31 +1300

    Show selected MIDI input name

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,12 @@ struct MidiInputsModel(Vec<ComboBoxItem>);
 
 impl slint::Model for MidiInputsModel {
     type Data = ComboBoxItem;
-
     fn row_count(&self) -> usize {
         self.0.len()
     }
-
     fn row_data(&self, row: usize) -> Option<Self::Data> {
         self.0.get(row).map(|x| x.clone())
     }
-
     fn model_tracker(&self) -> &dyn slint::ModelTracker {
         &()
     }
@@ -27,36 +24,26 @@ fn main() {
     let main_window = MainWindow::new().unwrap();
     main_window.set_window_title("PitchGrid-Continuum Companion".into());
 
-    let midi_inputs = vec![
-        ComboBoxItem {
-            text: "First".into(),
-        },
-        ComboBoxItem {
-            text: "Second".into(),
-        },
-        ComboBoxItem {
-            text: "Third".into(),
-        },
-    ];
-    let midi_inputs_model = MidiInputsModel(midi_inputs);
-    // let midi_inputs_model = MidiInputsModel(vec![
-    //     ComboBoxItem {
-    //         text: "First".into(),
-    //     },
-    //     ComboBoxItem {
-    //         text: "Second".into(),
-    //     },
-    //     ComboBoxItem {
-    //         text: "Third".into(),
-    //     },
-    // ]);
-    let midi_inputs_model = Rc::new(midi_inputs_model);
-    main_window.set_midi_inputs(slint::ModelRc::from(midi_inputs_model.clone()));
+        let midi_input_names = Rc::new(vec![
+                "First",
+                "Second",
+                "Third",
+        ]);
+        let midi_input_items: Vec<ComboBoxItem> = midi_input_names
+            .iter() // Use .iter() instead of .into_iter() to avoid consuming it
+            .map(|text| ComboBoxItem { text: (*text).into() })
+            .collect();    
+        let midi_inputs_model = MidiInputsModel(midi_input_items);
+        let midi_inputs_model = Rc::new(midi_inputs_model);
+        main_window.set_midi_inputs_model(slint::ModelRc::from(midi_inputs_model.clone()));
 
+        main_window.on_midi_input_changed(move |index: i32| {
+            if index >= 0 {
+                if let Some(name) = midi_input_names.clone().get(index as usize) {
+                    println!("MIDI input changed to {name}");
+                }
+            }
+        });
 
-    main_window.on_midi_input_changed(|index| {
-        // println!("MIDI input changed to {}", midi_inputs[index].text);
-        println!("MIDI input changed to {}", index);
-    });
-    main_window.run().unwrap();
+        main_window.run().unwrap();
 }
