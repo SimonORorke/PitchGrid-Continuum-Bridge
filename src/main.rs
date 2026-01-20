492b38285ae2eeb59487d6d9e267e3666ca100af
Simon O'Rorke <simon.ororke@outlook.com>
2026-01-21 12:47:33 +1300

    on_refresh_midi_outputs WIP

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@
 mod midi;
 
 use std::rc::Rc;
+use std::sync::Arc;
 use lazy_static::lazy_static;
+use slint::Weak;
 use midi::MidiManager;
 slint::include_modules!();
 
@@ -25,12 +27,38 @@ impl slint::Model for MidiInputsModel {
 
 lazy_static! {
     static ref MIDI_MANAGER: MidiManager = MidiManager::new();
+    static ref MIDI_OUTPUT_NAMES: Arc<Vec<String>> = Arc::new(Vec::new());
 }
 
 fn main() {
     let main_window = MainWindow::new().unwrap();
     main_window.set_window_title("PitchGrid-Continuum Companion".into());
 
+    let midi_output_names = set_midi_output_names(&main_window);
+
+    let main_window_weak = main_window.as_weak();
+    main_window.on_midi_output_changed(move |index: i32| {
+        on_midi_output_changed(midi_output_names.clone(), main_window_weak.clone(), index);
+    });
+
+    main_window.on_refresh_midi_outputs(move || {
+    });
+
+    main_window.run().unwrap();
+}
+
+fn on_midi_output_changed(midi_output_names: Rc<Vec<String>>, main_window_weak: Weak<MainWindow>, index: i32) {
+    if index >= 0 {
+        if let Some(main_window) = main_window_weak.upgrade() {
+            if let Some(name) = midi_output_names.get(index as usize) {
+                let message = format!("MIDI output changed to {name}");
+                main_window.invoke_show_message(message.into(), MessageType::Info);
+            }
+        }
+    }
+}
+
+fn set_midi_output_names(main_window: &MainWindow) -> Rc<Vec<String>> {
     let midi_output_names = Rc::new(MIDI_MANAGER.get_midi_output_names());
     let midi_output_items: Vec<ComboBoxItem> = midi_output_names
         .iter()
@@ -39,18 +67,5 @@ fn main() {
     let midi_outputs_model = MidiInputsModel(midi_output_items);
     let midi_outputs_model = Rc::new(midi_outputs_model);
     main_window.set_midi_outputs_model(slint::ModelRc::from(midi_outputs_model.clone()));
-    main_window.invoke_show_message(
-        "This crate is the main entry point for embedding user interfaces.".into(),
-        MessageType::Error);
-        // MessageType::Info);
-
-    main_window.on_midi_output_changed(move |index: i32| {
-        if index >= 0 {
-            if let Some(name) = midi_output_names.clone().get(index as usize) {
-                println!("MIDI output changed to {name}");
-            }
-        }
-    });
-
-    main_window.run().unwrap();
+    midi_output_names
 }
