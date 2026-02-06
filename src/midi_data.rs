ad1ee8f4fbf23ef1c16e888a290b59310f93c1e3
Simon O'Rorke <simon.ororke@outlook.com>
2026-02-06 15:53:45 +1300

    Midi refactor 2

--- src/midi_data.rs
+++ src/midi_data.rs
@@ -1,16 +1,18 @@
-﻿use std::any::Any;
-use std::fmt::Display;
-use midir::{MidiInputPort, MidiOutputPort};
+﻿use std::fmt::Display;
 
-pub struct Port {
+pub struct Port<T> {
     index: usize,
     name: String,
-    midi_port: Box<dyn Any>,
+    midi_port: T,
 }
 
-impl Port {
-    pub fn new<T: Any + 'static>(index: usize, name: String, midi_port: T) -> Self {
-        Self { index, name, midi_port: Box::new(midi_port) }
+impl<T> Port<T> {
+    pub fn new(index: usize, name: String, midi_port: T) -> Self {
+        Self {
+            index,
+            name,
+            midi_port,
+        }
     }
 
     pub fn index(&self) -> usize {
@@ -21,7 +23,42 @@ impl Port {
         &self.name
     }
 
-    pub fn midi_port<T: Any>(&self) -> &T {
-        &*self.midi_port.downcast_ref::<T>().unwrap()
+    pub fn midi_port(&self) -> &T {
+        &self.midi_port
     }
+
+    pub fn midi_port_mut(&mut self) -> &mut T {
+        &mut self.midi_port
+    }
+
+    pub fn into_midi_port(self) -> T {
+        self.midi_port
+    }
+}
+
+impl<T: Clone> Clone for Port<T> {
+    fn clone(&self) -> Self {
+        Self { index: self.index, name: self.name.clone(),
+            midi_port: self.midi_port.clone() }
+    }
+}
+
+impl<T> Display for Port<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[index: {}, name: {}]", self.index(), self.name())
+    }
+}
+
+struct Io<T> {
+    connected_port: Option<Port<T>>,
+    ports: Vec<Port<T>>,
+}
+
+impl<T> Io<T> {
+    fn new(ports: Vec<Port<T>>) -> Self {
+        Self { connected_port: None, ports }
+    }
+    fn connected_port(&self) -> Option<&Port<T>> { self.connected_port.as_ref() }
+    fn set_connected_port(&mut self, port: Port<T>) { self.connected_port = Some(port) }
+    fn ports(&self) -> &Vec<Port<T>> { self.ports.as_ref() }
 }
