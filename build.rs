9d541232e61568e63d1d5569ee77138a82456b39
Simon O'Rorke <simon.ororke@outlook.com>
2026-02-17 17:02:29 +1300

    Scalatrix compiles

--- build.rs
+++ build.rs
@@ -4,4 +4,20 @@ fn main() {
         slint_build::CompilerConfiguration::new().with_library_paths(vivi_ui::import_paths()),
     )
     .unwrap();
+    cxx_build::bridge("src/tuner.rs")  // returns a cc::Build
+        .file("scalatrix/src/affine_transform.cpp")
+        .file("scalatrix/src/label_calculator.cpp")
+        .file("scalatrix/src/lattice.cpp")
+        .file("scalatrix/src/linear_solver.cpp")
+        .file("scalatrix/src/main.cpp")
+        .file("scalatrix/src/mos.cpp")
+        .file("scalatrix/src/node.cpp")
+        .file("scalatrix/src/params.cpp")
+        .file("scalatrix/src/pitchset.cpp")
+        // .file("scalatrix/src/python_bindings.cpp")
+        .file("scalatrix/src/scale.cpp")
+        .include("scalatrix/include")
+        .std("c++17")
+        .compile("scalatrix");
+    println!("cargo:rerun-if-changed=scalatrix");
 }
--- src/tuner.rs
+++ src/tuner.rs
@@ -1,6 +1,16 @@
-﻿#[allow(unused)]
+﻿#[cxx::bridge(namespace = "sx")]
+mod ffi {
+    unsafe extern "C++" {
+        include!("scalatrix.hpp");
+        // type MOS;
+        // fn new_MOS(depth: i32, mode: i32, root_freq: f32, stretch: f32,
+        //            skew: f32, mode_offset: i32, steps: i32) -> UniquePtr<MOS>;
+        //
+    }
+}
 
+#[allow(unused)]
 pub fn on_tuning_changed(depth: i32, mode: i32, root_freq: f32, stretch: f32,
                           skew: f32, mode_offset: i32, steps: i32) {
-
+    // let mos = unsafe { ffi::new_MOS(depth, mode, skew as f64, stretch as f64, 1) };
 }
