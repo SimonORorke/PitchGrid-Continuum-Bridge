04c4338dcd789b2b1e6d2c281e856414c3f2c2a6
Simon O'Rorke <simon.ororke@outlook.com>
2026-03-26 13:27:54 +1300

    Max turns off initial rounding. Wider pitch table number.

--- src/tuner.rs
+++ src/tuner.rs
@@ -332,17 +332,22 @@ fn on_tuning_updated() {
 /// However, dues to a bug in the EaganMatrix firmware,
 /// when sent before or after sending a new tuning, the instrument's octave shifting,
 /// with buttons or pedals, will not work.
-/// Rounding Max send Rounding Mode Normal in conjunction with Rounding Rate 127 (the maximum).
-/// This effectively enforces initial rounding, even when the Initial Rounding parameter is off,
-/// without breaking octave shifting. In addition, it prevents the pitch from being changed
-/// by subsequent motion of the finger on the fingerboard.
+/// Rounding Max turns off Initial Rounding and sends Rounding Mode Normal in conjunction with
+/// Rounding Rate 127 (the maximum). This effectively enforces initial rounding, even though the
+/// Initial Rounding parameter is turned off, without breaking octave shifting. In addition, it
+/// prevents the pitch from being changed by subsequent motion of the finger on the fingerboard.
 fn send_rounding_params(rounding: Rounding) {
     match rounding {
         Rounding::None => {}
         Rounding::Initial => {
+            // Turn on Rounding Initial
             Midi::send_control_change(1, 28, 127); // RndIni
         }
         Rounding::Max => {
+            // Turn off Rounding Initial
+            // Needs to be done to avoid the pitch shift bug in the EaganMatrix firmware
+            // happening for presets that have Initial Rounding turned on by default.
+            Midi::send_control_change(1, 28, 0); // RndIni
             // Rounding Mode Normal
             Midi::send_matrix_poke(10, 0); // RoundMode
             // Max Rounding Rate
