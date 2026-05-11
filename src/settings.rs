cf0b3cf08cdb80f0b938588fe529879a8712f281
Simon O'Rorke <simon.ororke@outlook.com>
2026-05-11 17:59:46 +1200

    Settings refactor: PathBuf

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,6 @@
 ﻿use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 use app_info::APP_TITLE;
 
@@ -31,27 +32,26 @@ impl Settings {
         }
     }
 
-    fn get_app_config_folder_path(&self) -> String {
-        let mut path = String::new();
+    fn get_app_config_folder_path(&self) -> Result<PathBuf, Box<dyn Error>> {
         if let Some(base_dirs) = directories::BaseDirs::new() {
-            let config_folder_path = base_dirs.config_dir().to_str().unwrap().to_string();
-            path =
-                format!("{}/{}", config_folder_path, APP_TITLE);
+            let config_folder_path = base_dirs.config_dir().to_path_buf();
+            return Ok(config_folder_path.join(APP_TITLE));
         }
-        path
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::NotFound, NO_SETTINGS_FOLDER_PATH)))
     }
 
-    fn get_path(&self) -> String {
-        format!("{}/Settings.toml", self.get_app_config_folder_path())
+    fn get_path(&self) -> Result<PathBuf, Box<dyn Error>> {
+        Ok(self.get_app_config_folder_path()?.join("Settings.toml"))
     }
 
     pub fn read_from_file(&mut self) -> Result<(), Box<dyn Error>> {
-        let path = self.get_path();
+        let path = self.get_path()?;
         let toml_str = match fs::read_to_string(&path) {
             Ok(s) => s,
             Err(e) if e.kind() == std::io::ErrorKind::NotFound => return Ok(()),
             Err(e) => {
-                let msg = format!("Error reading settings file '{}': {}", path.clone(), e);
+                let msg = format!("Error reading settings file '{:?}': {}", path, e);
                 return Err(Box::new(std::io::Error::new(e.kind(), msg)));
             }
         };
@@ -79,25 +79,24 @@ impl Settings {
     }
 
     pub fn write_to_file(&mut self) -> Result<(), Box<dyn Error>> {
-        let path = self.get_path();
-        if path.is_empty() {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::NotFound, "Settings path is empty")));
-        }
-        let app_config_folder_path = self.get_app_config_folder_path();
-        let app_config_folder_exists = fs::exists(&app_config_folder_path)?;
-        if !app_config_folder_exists {
-            fs::create_dir(app_config_folder_path)?;
+        let path = self.get_path()?;
+        // It is safe to unwrap as get_path() would have thrown an error if a parent folder
+        // could not be specified.
+        let parent_folder_path = path.parent().unwrap();
+        if !parent_folder_path.exists() {
+            fs::create_dir(parent_folder_path)?;
         }
         // Serialise settings.
         let toml = toml::to_string(&self)?;
         if let Err(e) = fs::write(&path, toml) {
             return Err(
                 Box::new(std::io::Error::new(e.kind(),
-                format!("Error writing settings file '{path}': {e}"))));
+                format!("Error writing settings file '{:?}': {e}", path))));
         }
         // println!("Settings.write_to_file: self.midi_input_device = {}; self.midi_output_device = {}; \
         // self.pitch_table = {:?};", self.midi_input_device, self.midi_output_device, self.pitch_table);
         Ok(())
     }
 }
+
+const NO_SETTINGS_FOLDER_PATH: &str = "A valid settings folder path cannot be specified.";
