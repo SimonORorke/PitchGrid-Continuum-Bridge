81ef1816e37c0cd63bb1541b1feb7a169b921281
Simon O'Rorke <simon.ororke@outlook.com>
2026-05-20 16:26:49 +1200

    Fixed Settings test

--- tests/settings_tests.rs
+++ tests/settings_tests.rs
@@ -2,6 +2,7 @@ mod temp_path_finder;
 
 use googletest::assert_that;
 use googletest::matchers::{eq, ok};
+use pitchgrid_continuum::i_settings::ISettings;
 use pitchgrid_continuum::settings::Settings;
 use temp_path_finder::TempPathFinder;
 
@@ -11,10 +12,10 @@ fn persist() {
     let mut settings = Settings::new();
     let temp_path_finder = TempPathFinder::new();
     settings.set_system_path_finder(Box::new(temp_path_finder.clone()));
-    settings.pitch_table = PITCH_TABLE;
+    settings.set_pitch_table(PITCH_TABLE);
     assert_that!(settings.write_to_file(), ok(()));
     settings = Settings::new();
     settings.set_system_path_finder(Box::new(temp_path_finder.clone()));
     assert_that!(settings.read_from_file(), ok(()));
-    assert_that!(settings.pitch_table, eq(PITCH_TABLE));
+    assert_that!(settings.pitch_table(), eq(PITCH_TABLE));
 }
