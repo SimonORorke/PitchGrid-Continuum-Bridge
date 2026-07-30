3586ea57313bef89efbe7e1385f6a1feb13ef916
Simon O'Rorke <simon.ororke@outlook.com>
2026-07-31 08:30:53 +1200

    Comment

--- app_info/src/lib.rs
+++ app_info/src/lib.rs
@@ -1,5 +1,5 @@
 // The CARGO_PKG_... environment variables are set in the workspace.package section of the
-// Cargo.toml file.
+// top-level Cargo.toml file.
 pub const APP_TITLE: &str = env!("CARGO_PKG_DESCRIPTION");
 pub const COPYRIGHT: &str = "Copyright © 2026 Simon O'Rorke";
 pub const DOCUMENTATION_LINK: &str =
